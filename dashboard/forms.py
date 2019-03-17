a0de98982d59860e0b22eff063135d9344a0c8ee
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-03-17 17:10:41 -0400

    Implement login form using the Frontend template Implement the login form with a custom form class that integrates the Frontend subteam's html and css

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import ugettext, ugettext_lazy as _
+from django.forms.widgets import PasswordInput, TextInput
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
@@ -13,4 +16,11 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email')
-        
+
+class CustomAuthForm(AuthenticationForm):
+    username = forms.CharField(label='', max_length=254, widget=TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(label='', widget=PasswordInput(attrs={'placeholder': _("Password")}))
+    class Media:
+        css = {
+            'all': ('dashboard/login-form.css',),
+        }
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .forms import CustomAuthForm
 
 from . import views
 
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='dashboard/login.html'), name='login2'),
+    path('login/', auth_views.LoginView.as_view(template_name='dashboard/login.html', authentication_form=CustomAuthForm), name='login'),
 ]
