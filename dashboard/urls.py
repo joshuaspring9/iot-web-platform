b6392021a3458a8ac758baa896b4f0e98f6602de
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-03-17 19:14:03 -0400

    Move home URL into the dashboard app

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 from .forms import CustomAuthForm
 
 from . import views
 
 app_name='dashboard'
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='dashboard/home.html'), name='home'),
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='dashboard/login.html', authentication_form=CustomAuthForm), name='login'),
 ]
--- iot-web-platform/urls.py
+++ iot-web-platform/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('dashboard.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='iot-web-platform/home.html'), name='home'),
+    path('', include('dashboard.urls')),
 ]
