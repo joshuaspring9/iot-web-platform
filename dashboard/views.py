3136473704ab7c4998630a18b7fb5a249d7eee06
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-03-17 18:22:43 -0400

    Remove unneeded import from views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from .forms import CustomUserCreationForm
 
 # Create your views here.
 
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from .forms import CustomUserCreationForm
 
 
 class SignUp(generic.CreateView):
