c2443cf7b8c2a6aeb8401852a9a82b9477e7eb8e
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-03-17 20:09:53 -0400

    Implement about page using the Frontend template

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,6 +8,7 @@ from . import views
 app_name='dashboard'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='dashboard/home.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='dashboard/about.html'), name='about'),
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='dashboard/login.html', authentication_form=CustomAuthForm), name='login'),
 ]
