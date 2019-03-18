6ff5c8f0e181aadc82e9c91b01632029561b7ae2
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-03-18 13:09:39 -0400

    Use signals to pull in social profile picture URL Capture the user_signed_up signal when a user signs up to populate the profile picture URL in the UserProfile model.

--- dashboard/models.py
+++ dashboard/models.py
@@ -9,3 +9,12 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.email
         
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, primary_key=True, verbose_name='user', related_name='profile', on_delete=models.CASCADE)
+    avatar_url = models.CharField(max_length=256, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.email
+
+    class Meta():
+        db_table = 'user_profile'
--- dashboard/signals.py
+++ dashboard/signals.py
@@ -3,7 +3,8 @@ from django.dispatch import receiver
 from .models import UserProfile
 
 @receiver(user_signed_up)
-def social_login_set_profilepic(sender, **kwargs):
-    user = kwargs['user']
-    profile = UserProfile(user=user, avatar_url="testing...")
-    profile.save()
+def social_login_set_profilepic(request, sociallogin, user, **kwargs):
+    if sociallogin.account.provider == 'google':
+        picture_url = sociallogin.account.extra_data['picture']
+        profile = UserProfile(user=user, avatar_url=picture_url)
+        profile.save()
