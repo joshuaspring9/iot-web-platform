7358c9ccd665777488842d26090313e8489e7d66
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-03-28 14:48:13 -0400

    Incorporate Django REST framework Will be needed for data file upload.

--- iot-web-platform/settings.py
+++ iot-web-platform/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'rest_framework',
     'allauth', 
     'allauth.account',
     'allauth.socialaccount',
@@ -133,6 +134,13 @@ ACCOUNT_FORMS = {
     'signup': 'dashboard.forms.CustomUserCreationForm'
 }
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 try:
     from .local_settings import *
