f605acf16b8beccc610621f37f5128d79ea887a6
Varun Pilly <vpilly@stevens.edu>
2019-03-28 14:50:41 -0400

    fixed readme

--- iot-web-platform/local_settings_dist.py
+++ iot-web-platform/local_settings_dist.py
@@ -11,8 +11,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_dashboard',
-        'USER': 'django',
-        'PASSWORD': 'Ilikepython',
+        'USER': 'root',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
     }
 }
