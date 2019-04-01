a722e82128dc2574e8efdae02eaf781476a9d89f
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-03-31 21:15:46 -0400

    Define model for datafiles

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class DataFile(models.Model):
+
+    data_file = models.FileField(upload_to="data-files")
+    processed = models.BooleanField()
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return "File Name: " + self.data_file + " Start Time: " + self.start_time + " End Time: " + self.end_time
