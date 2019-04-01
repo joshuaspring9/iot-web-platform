aac74d379552d0314d056afeb19e0b4d27b4dd6b
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-03-31 22:25:49 -0400

    Add local media directory to settings

--- iot-web-platform/settings.py
+++ iot-web-platform/settings.py
@@ -133,6 +133,8 @@ ACCOUNT_FORMS = {
     'signup': 'dashboard.forms.CustomUserCreationForm'
 }
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 try:
     from .local_settings import *
