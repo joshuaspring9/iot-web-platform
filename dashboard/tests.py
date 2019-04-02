33ac29d38d97835d5dfe8ff5d7a37bca11a3cc3e
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-02 14:31:07 -0400

    Add tests for UserProfile model

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import UserProfile, CustomUser
 
 # Create your tests here.
+class UserProfileTestCase(TestCase):
+    '''
+    Tests the UserProfile model
+    '''
+
+    def setUp(self):
+        self.user = CustomUser.objects.create(username='test', email="test@test.com")
+        UserProfile.objects.create(user=self.user, avatar_url='http://localhost')
+
+    def test_avatar_url(self):
+        profile = UserProfile.objects.get(user=self.user)
+        self.assertEqual(profile.avatar_url, 'http://localhost')
+        self.assertEqual(profile.has_profile_image(), False)
+        self.assertEqual(profile.has_avatar_url(), True)
+        self.assertEqual(str(profile), "test@test.com")
