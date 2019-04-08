ec449b4c17ae9a1b3bc38990b558845712a9b2e3
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-07 22:18:53 -0400

    Use post_save signal to call ML subteam's script Framework to call the ML subteam's script to process a newly uploaded data file.

---
+++ api/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import DataFile
+
+@receiver(post_save, sender=DataFile)
+def create_profile(sender, instance, created, **kwargs):
+    """Call an outside script whenever a DataFile is created to process the file."""
+    if created:
+        # this is where we want to call the ML sub-team's script
+        pass
