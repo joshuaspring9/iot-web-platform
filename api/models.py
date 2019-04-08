e4d7dc2de505d9687879ca205aea5135c60578c7
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-07 22:10:27 -0400

    Add method to determine whether a datafile is processed

--- api/models.py
+++ api/models.py
@@ -12,5 +12,8 @@ class DataFile(models.Model):
     start_time = models.DateTimeField(null=True, blank=True)
     end_time = models.DateTimeField(null=True, blank=True)
 
+    def is_processed(self):
+        return self.processed      
+
     def __str__(self):
         return "File Name: " + self.data_file + " Start Time: " + self.start_time + " End Time: " + self.end_time
