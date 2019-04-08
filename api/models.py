623f3a3694565bef38214d6d4b60cdf9a34bbf05
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-07 20:00:08 -0400

    Update data file model to hold the processed data

--- api/models.py
+++ api/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 class DataFile(models.Model):
 
-    data_file = models.FileField(upload_to="media/data-files")
+    data_file = models.FileField(upload_to='media/data-files')
     processed = models.BooleanField()
+    processed_file = models.FileField(upload_to='media/data-files')
     start_time = models.DateTimeField(null=True, blank=True)
     end_time = models.DateTimeField(null=True, blank=True)
 
