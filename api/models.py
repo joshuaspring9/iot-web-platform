4d2190ba40e87db41e68acf4c7c5a78c2cba3145
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-07 22:00:25 -0400

    Implement data file validation

--- api/models.py
+++ api/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
 class DataFile(models.Model):
 
-    data_file = models.FileField(upload_to='media/data-files')
+    # Note that while only PCAP extensions, are allowed, ANY file can be renamed to .pcap and be accepted.
+    # TODO: implement stricter file validation
+    data_file = models.FileField(upload_to='media/data-files', validators=[FileExtensionValidator(allowed_extensions=['pcap'])])
     processed = models.BooleanField()
     processed_file = models.FileField(upload_to='media/data-files')
     start_time = models.DateTimeField(null=True, blank=True)
