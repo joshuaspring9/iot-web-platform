788423dcc834d3a8ba3c5fdf53a9922b43b247bb
Varun Pilly <vpilly@stevens.edu>
2019-04-08 20:53:13 -0400

    created scripts

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+FILE="./.djangosql"
+
+while true
+do
+    read -p "Do you want to rerun the setup? [Y/n] " yn
+    case $yn in
+        [yY]* ) echo Resetting
+                chmod 0700 $FILE
+                mysql_config_editor remove --login-path=django
+                break
+                ;;
+        * ) echo Abort.
+            exit 0
+            ;;
+    esac
+done
+
+> $FILE
+
+read -p "Enter your mysql user(root is default): " user
+[ -z "$user" ] && user="root"
+read -p "Enter your mysql password: " pass
+
+echo "[django]" >> $FILE
+echo "user=${user}" >> $FILE
+echo "password=${pass}" >> $FILE
+
+chmod 0400 $FILE
+
+/usr/bin/expect <<EOF
+    spawn mysql_config_editor set --login-path=django --host=localhost --user=$user --password
+    expect "Enter password: "
+    send "${pass}\r"
+    expect EOF
+EOF
+echo Setup Complete
