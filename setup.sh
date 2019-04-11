a742ca61a1328a2b5cb71955273f8a1b2c3df588
Varun Pilly <vpilly@stevens.edu>
2019-04-11 15:09:42 -0400

    fixed malfunctioning script

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ do
     read -p "Do you want to rerun the setup? [Y/n] " yn
     case $yn in
         [yY]* ) echo Resetting
-                [ -f FILE ] && chmod 0700 $FILE
+                [ -f $FILE ] && chmod 0700 $FILE
                 mysql_config_editor remove --login-path=django
                 break
                 ;;
@@ -21,7 +21,13 @@ done
 
 read -p "Enter your mysql user(root is default): " user
 [ -z "$user" ] && user="root"
-read -p "Enter your mysql password: " pass
+
+while true
+do
+read -sp "Enter your mysql password: " pass
+[ -z "$pass" ] || break
+done
+
 
 echo "[django]" >> $FILE
 echo "user=${user}" >> $FILE
