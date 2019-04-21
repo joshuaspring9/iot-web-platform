e1f9b54c5c687b9d9bf0ecce0a42aeb75c64f5df
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-21 19:51:02 -0400

    Allow editing of devices in the admin interface

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import DataFile
+from .models import DataFile, SmartHomeDevice
 
 admin.site.register(DataFile)
+admin.site.register(SmartHomeDevice)
