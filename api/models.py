de4a2c03446ffa85065f1f3e979a6ecac1d4b26a
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-21 16:54:04 -0400

    Add a smart home device model Allows each data file to be associated with multiple smart devices that the capture contains in a many to many relationship.

--- api/models.py
+++ api/models.py
@@ -2,18 +2,36 @@ from django.db import models
 from django.core.validators import FileExtensionValidator
 
 # Create your models here.
-class DataFile(models.Model):
+class SmartHomeDevice(models.Model):
+    '''
+    Class to represent a smart home device.  Used with data files to represent the devices captured.
+    '''
 
+    name = models.CharField(max_length=256)
+    image = models.ImageField(blank=True, null=True, upload_to="device-pictures")
+
+    def has_device_image(self):
+        return self.image != ""
+
+    def __str__(self):
+        return self.name
+
+
+class DataFile(models.Model):
+    '''
+    Class to represent a network data capture file
+    '''
     # Note that while only PCAP extensions, are allowed, ANY file can be renamed to .pcap and be accepted.
     # TODO: implement stricter file validation
     data_file = models.FileField(upload_to='media/data-files', validators=[FileExtensionValidator(allowed_extensions=['pcap'])])
+    devices_captured = models.ManyToManyField(SmartHomeDevice)
     processed = models.BooleanField()
-    processed_file = models.FileField(upload_to='media/data-files', validators=[FileExtensionValidator(allowed_extensions=['txt'])])
+    processed_file = models.FileField(upload_to='media/data-files', null=True, blank=True, validators=[FileExtensionValidator(allowed_extensions=['txt'])])
     start_time = models.DateTimeField(null=True, blank=True)
     end_time = models.DateTimeField(null=True, blank=True)
 
     def is_processed(self):
-        return self.processed      
+        return self.processed
 
     def __str__(self):
         return "File Name: " + self.data_file + " Start Time: " + self.start_time + " End Time: " + self.end_time
