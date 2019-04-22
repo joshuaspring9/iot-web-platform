f5901cf2bd6b7846d65a06967c05558ec388f554
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-21 21:28:40 -0400

    Implement custom permission for the API views Allows interacting with an API view if the user has the proper model permissions, is an administrator, or is authenticated with an OAuth 2 token with both read and write scope.

--- api/views.py
+++ api/views.py
@@ -3,13 +3,14 @@ from dashboard.models import CustomUser, UserProfile
 from .models import DataFile, SmartHomeDevice
 from rest_framework import viewsets
 from .serializers import UserSerializer, UserProfileSerializer, DataFileSerializer, SmartHomeDeviceSerializer
-
+from .permissions import IsAdminOrHasModelPermissionsOrTokenHasScope
 # Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
+    permission_classes = [IsAdminOrHasModelPermissionsOrTokenHasScope]
     queryset = CustomUser.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
@@ -17,6 +18,7 @@ class SmartHomeDeviceViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows smart home devices to be viewed or edited.
     """
+    permission_classes = [IsAdminOrHasModelPermissionsOrTokenHasScope]
     queryset = SmartHomeDevice.objects.all()
     serializer_class = SmartHomeDeviceSerializer
 
@@ -24,5 +26,6 @@ class DataFileViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows data files to be viewed or edited.
     """
+    permission_classes = [IsAdminOrHasModelPermissionsOrTokenHasScope]
     queryset = DataFile.objects.all()
     serializer_class = DataFileSerializer
