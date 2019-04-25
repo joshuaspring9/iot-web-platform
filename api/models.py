178a0e9a9fd1a23390e05fe91a8aef22cdf75ab7
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-25 14:33:48 -0400

    Add data file hash to prevent duplicate uploads

--- api/models.py
+++ api/models.py
@@ -25,6 +25,7 @@ class DataFile(models.Model):
     # Note that while only PCAP extensions, are allowed, ANY file can be renamed to .pcap and be accepted.
     # TODO: implement stricter file validation
     data_file = models.FileField(upload_to='media/data-files', validators=[FileExtensionValidator(allowed_extensions=['pcap'])])
+    data_file_hash = models.CharField(max_length=35, unique=True)
     devices_captured = models.ManyToManyField(SmartHomeDevice)
     processed = models.BooleanField()
     processed_file = models.FileField(upload_to='media/data-files', null=True, blank=True, validators=[FileExtensionValidator(allowed_extensions=['txt'])])
@@ -33,6 +34,14 @@ class DataFile(models.Model):
 
     def is_processed(self):
         return self.processed
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if self.data_file_hash == '':
+            import hashlib
+            self.data_file_hash = hashlib.md5(self.data_file)
+        super().save(force_insert, force_update, using,
+             update_fields)
 
     def __str__(self):
         return "File Name: " + self.data_file + " Start Time: " + self.start_time + " End Time: " + self.end_time
