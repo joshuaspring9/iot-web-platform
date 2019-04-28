1c2ad467d8ddcca292472f49e3c96d2b1e9a7e22
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-28 15:51:38 -0400

    Rename method to process datafile

--- api/signals.py
+++ api/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from .models import DataFile
 
 @receiver(post_save, sender=DataFile)
-def create_profile(sender, instance, created, **kwargs):
+def process_datafile(sender, instance, created, **kwargs):
     """Call an outside script whenever a DataFile is created to process the file."""
     if created:
         # this is where we want to call the ML sub-team's script
