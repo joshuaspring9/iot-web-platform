f4de8247b0ec92a41951a57b76996c8ece54c3cc
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-28 18:56:26 -0400

    Add DataFile pre_save signal Computes the hash of the data file if the form did not already compute the hash.

--- api/signals.py
+++ api/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
 from .models import DataFile
 
@@ -8,3 +8,17 @@ def process_datafile(sender, instance, created, **kwargs):
     if created:
         # this is where we want to call the ML sub-team's script
         pass
+
+@receiver(pre_save, sender=DataFile)
+def populate_datafile_hash(sender, instance, **kwargs):
+    """
+    Calculates the hash of the uploaded datafile before saving and populates the respective field in the model, if it hasn't already been populated by the Admin form or API.
+    """
+    if sender == DataFile and not instance.data_file_hash:
+        print("got here")
+        import hashlib
+        sha256_hash = hashlib.sha256()
+        # Read and update hash string value in blocks of 4K
+        for chunk in instance.data_file.chunks():
+            sha256_hash.update(chunk)
+        instance.data_file_hash = sha256_hash.hexdigest()[:32]
