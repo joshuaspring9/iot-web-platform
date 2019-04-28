f8654fd5ab5350b92ea17b3038064ea9d1c86079
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-28 18:40:36 -0400

    Add a form for DataFile model Automatically computes the hash of the uploaded file, raising an error if that file has already been uploaded.  Stores the computed hash in the database otherwise.

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
+from .forms import DataFileForm
 
 # Register your models here.
 from .models import DataFile, SmartHomeDevice
 
 class DataFileAdmin(admin.ModelAdmin):
-    exclude = ('data_file_hash',)
+    form = DataFileForm
+    # override the save method to include the data_file_hash
+    def save_model(self, request, obj, form, change):
+        obj.data_file_hash = form.cleaned_data['data_file_hash']
+        obj.save()
 
 admin.site.register(DataFile, DataFileAdmin)
 admin.site.register(SmartHomeDevice)
