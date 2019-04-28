587feb34bb81996d062aeaa16009d6757785b2c8
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-28 15:50:02 -0400

    Hide data file hash from admin interface

--- api/admin.py
+++ api/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import DataFile, SmartHomeDevice
 
-admin.site.register(DataFile)
+class DataFileAdmin(admin.ModelAdmin):
+    exclude = ('data_file_hash',)
+
+admin.site.register(DataFile, DataFileAdmin)
 admin.site.register(SmartHomeDevice)
