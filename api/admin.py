a3be9474e9b6236607ed7398f12fd11421076f0d
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-29 01:20:56 -0400

    Allow the data file to remain the same Allows the data file to remain the same when using the DataFile form while making other changes.

--- api/admin.py
+++ api/admin.py
@@ -8,7 +8,8 @@ class DataFileAdmin(admin.ModelAdmin):
     form = DataFileForm
     # override the save method to include the data_file_hash
     def save_model(self, request, obj, form, change):
-        obj.data_file_hash = form.cleaned_data['data_file_hash']
+        if 'data_file_hash' in form.cleaned_data:
+            obj.data_file_hash = form.cleaned_data['data_file_hash']
         obj.save()
 
 admin.site.register(DataFile, DataFileAdmin)
--- api/forms.py
+++ api/forms.py
@@ -2,11 +2,13 @@ from django import forms
 from .models import DataFile
 
 class DataFileForm(forms.ModelForm):
+
     class Meta:
         model = DataFile
         exclude = ('data_file_hash', )
 
     def clean(self):
+        self.clean_data = super().clean()
         import hashlib
         sha256_hash = hashlib.sha256()
         # Read and update hash string value in blocks of 4K
@@ -15,6 +17,6 @@ class DataFileForm(forms.ModelForm):
         # stash the hash in the cleaned data so it can be inserted into the database
         self.cleaned_data['data_file_hash'] = sha256_hash.hexdigest()[:32]
         # if the hash already exists, we have a duplicate file, so raise a form validation error
-        if DataFile.objects.filter(data_file_hash=self.cleaned_data['data_file_hash']).exists():
-            raise forms.ValidationError({'data_file': "That file has already been uploaded."})
+        if (self.instance.pk == None or (self.instance.pk != None and self.cleaned_data['data_file_hash'] != self.instance.data_file_hash)) and DataFile.objects.filter(data_file_hash=self.cleaned_data['data_file_hash']).exists():
+            raise forms.ValidationError({'data_file':"That file has already been uploaded."})
         return self.cleaned_data
