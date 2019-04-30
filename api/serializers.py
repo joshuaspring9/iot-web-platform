737a0ce3d9fbd314d53a51f0ae765560055f68dd
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-04-30 17:13:15 -0400

    Introduce file validation in DataFile serializer Almost exactly the same as the validation in the admin form.  Allows a nice error message to be sent when a duplicate data file is uploaded through the API.

--- api/serializers.py
+++ api/serializers.py
@@ -26,6 +26,21 @@ class DataCapturingDeviceSerializer(serializers.ModelSerializer):
 
 class DataFileSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.ReadOnlyField()
+
+    def validate(self, data):
+        if 'data_file' in data and data['data_file']:
+            import hashlib
+            sha256_hash = hashlib.sha256()
+            # Read and update hash string value in blocks of 4K
+            for chunk in data['data_file'].chunks():
+                sha256_hash.update(chunk)
+            # stash the hash in the data so it can be inserted into the database
+            data['data_file_hash'] = sha256_hash.hexdigest()[:32]
+            # if the hash already exists, we have a duplicate file, so raise a form validation error
+            if (self.instance is None or (self.instance is not None and data['data_file_hash'] != data.data_file_hash)) and DataFile.objects.filter(data_file_hash=data['data_file_hash']).exists():
+                raise serializers.ValidationError({'data_file':"That file has already been uploaded."})
+        return data
+
     class Meta:
         model = DataFile
         exclude = ('data_file_hash',)
