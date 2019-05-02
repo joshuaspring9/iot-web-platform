234d078f3f498a3fcc7e41f7ca0496527ebfa7df
Joshua Zeitlinger <jzeitlin@stevens.edu>
2019-05-02 00:03:56 -0400

    Add new account management page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 from .forms import CustomAuthForm
 
 from . import views
@@ -7,5 +8,6 @@ from . import views
 app_name = 'dashboard'
 urlpatterns = [
     path('accounts/profile/picture/', views.UpdateProfilePic.as_view(), name='profilepic'),
-    path('dashboard/', views.DashboardView.as_view(), name='home')
+    path('dashboard/', views.DashboardView.as_view(), name='home'),
+    path('myaccount/', TemplateView.as_view(template_name='dashboard/account.html'), name='myaccount')
 ]
